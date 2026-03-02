ee99347085d9c178e2d48f16e1ecf8b7b5b0a504
pafkyag <mailtonapkd@gmail.com>
2026-03-02 11:18:09 +0000

    reading a file

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,4 +1,11 @@
 ﻿
 using Palindrome;
 
-Util.IsPalindrome("madam"); // true
+string fileToCheck = @"palindrome.txt";
+IEnumerable<string> lines = File.ReadLines(fileToCheck);
+foreach (string line in lines)
+{
+    Console.WriteLine($"Is '{line}' a palindrome? {Util.IsPalindrome(line)}");
+}
+
+Console.ReadKey();
